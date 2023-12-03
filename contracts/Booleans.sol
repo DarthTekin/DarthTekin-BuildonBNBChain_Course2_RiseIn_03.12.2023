9b94b55696a63377b07010d8ba1a73bd64cac6fc
DarthTekin <tekinteks@gmail.com>
2023-12-03 18:43:50 +0300

    Booleans.sol completed and added

---
+++ contracts/Booleans.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+contract Booleans
+{
+    bool public isOwner = true;
+
+    function getVar() public view returns(bool)
+    {
+        return isOwner;
+    }
+
+    function setVar(bool newValue) public
+    {
+        isOwner = newValue;
+    }
+}
--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -4,5 +4,10 @@ pragma solidity ^0.8.23;
 
 contract HelloWorld 
 {
-    string public text="Hello, World";
+    string public text = "Hello, World";
+
+    function getText() public view returns(string memory)
+    {
+        return text;
+    }
 }
